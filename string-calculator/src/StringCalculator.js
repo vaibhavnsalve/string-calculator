0e28a7709042bed239018188d59eb6d72a314c4e
Vaibhav Salve <vaibhavnsalve@gmail.com>
2024-10-19 18:03:16 +0530

    Implement support for custom delimiters

--- string-calculator/src/StringCalculator.js
+++ string-calculator/src/StringCalculator.js
@@ -1,13 +1,19 @@
 
-    export const add = (numbers) => {
-        if (!numbers) return 0;
-      
-        // Handle comma and newline as delimiters
-        const delimiters = /[,\n]/;
-        const numArr = numbers.split(delimiters).map(Number);
-        
-        return numArr.reduce((acc, curr) => acc + curr, 0);
-      };
-      
+export const add = (numbers) => {
+    if (!numbers) return 0;
+  
+    let delimiter = /[,\n]/;
+  
+    // Check for custom delimiter
+    if (numbers.startsWith("//")) {
+      const delimiterEndIndex = numbers.indexOf("\n");
+      delimiter = new RegExp(numbers.substring(2, delimiterEndIndex));
+      numbers = numbers.substring(delimiterEndIndex + 1);
+    }
+  
+    const numArr = numbers.split(delimiter).map(Number);
+    return numArr.reduce((acc, curr) => acc + curr, 0);
+  };
+  
 
   
