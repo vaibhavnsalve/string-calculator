3e1303bfb7f1ee1a122242f10948c3dcfd5f78d8
Vaibhav Salve <vaibhavnsalve@gmail.com>
2024-10-19 17:51:41 +0530

    Add test for custom delimiters

--- string-calculator/src/StringCalculator.test.js
+++ string-calculator/src/StringCalculator.test.js
@@ -21,4 +21,8 @@ test('should return the sum of two comma-separated numbers', () => {
   test('should handle new lines as delimiters', () => {
     expect(add("1\n2,3")).toBe(6);
   });
+
+  test('should support different delimiters', () => {
+    expect(add("//;\n1;2")).toBe(3);
+  });
   
