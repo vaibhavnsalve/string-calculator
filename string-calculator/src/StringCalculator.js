658ed7acfd03b14d4c2e485065ab83141e203615
Vaibhav Salve <vaibhavnsalve@gmail.com>
2024-10-19 17:49:29 +0530

    Implement multiple numbers and new line delimiters in add function

--- string-calculator/src/StringCalculator.js
+++ string-calculator/src/StringCalculator.js
@@ -1,5 +1,13 @@
-export const add = (numbers) => {
-    if (!numbers) return 0;
-    return parseInt(numbers, 10);
-  };
+
+    export const add = (numbers) => {
+        if (!numbers) return 0;
+      
+        // Handle comma and newline as delimiters
+        const delimiters = /[,\n]/;
+        const numArr = numbers.split(delimiters).map(Number);
+        
+        return numArr.reduce((acc, curr) => acc + curr, 0);
+      };
+      
+
   
