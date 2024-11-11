d18ec290a0b41cea68f466d28b02bfb43d3c8a39
Vaibhav Salve <vaibhavnsalve@gmail.com>
2024-11-11 10:25:57 +0530

    ui enhansment frontend special task

---
+++ src/StringCalculator.test.js
@@ -0,0 +1,36 @@
+import { add } from './StringCalculator';
+
+describe('String Calculator TDD Kata', () => {
+  test('should return 0 for an empty string', () => {
+    expect(add("")).toBe(0);
+  });
+
+  test('should return the number for a single number string', () => {
+    expect(add("1")).toBe(1);
+  });
+});
+
+test('should return the sum of two comma-separated numbers', () => {
+    expect(add("1,2")).toBe(3);
+  });
+  
+  test('should handle an unknown amount of numbers', () => {
+    expect(add("1,2,3")).toBe(6);
+  });
+  
+  test('should handle new lines as delimiters', () => {
+    expect(add("1\n2,3")).toBe(6);
+  });
+
+  test('should support different delimiters', () => {
+    expect(add("//;\n1;2")).toBe(3);
+  });
+
+  test('should throw an exception for negative numbers', () => {
+    expect(() => add("1,-2,3")).toThrow("negative numbers not allowed: -2");
+  });
+  
+  test('should show all negative numbers in the exception message', () => {
+    expect(() => add("1,-2,-3")).toThrow("negative numbers not allowed: -2, -3");
+  });
+  
